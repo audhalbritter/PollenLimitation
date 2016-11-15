234381555e83c106a9512d27def2dbc0c429fa24
audhalbritter <me@audhalbritter.com>
2016-11-15 21:37:56 +0100

    Calculate cumsum

--- ClimateData.R
+++ ClimateData.R
@@ -13,36 +13,16 @@ dailyT <- dailyTemperature %>%
 
 #### calcualte cumsum #### 
 climateData <- dailyT %>% 
+  filter(site %in% c("Skjellingahaugen", "Gudmedalen", "Veskre", "Rambera")) %>% 
+  mutate(site = plyr::mapvalues(site, from = c("Skjellingahaugen", "Gudmedalen", "Veskre", "Rambera"), to = c("SKJ", "GUD", "VES", "RAM"))) %>% 
+  filter(date > (ymd("2015.1.1")), date < (ymd("2015.12.31"))) %>%
+  filter(logger == "temp30cm") %>% 
   mutate(temp = replace(value, is.na(value), 0)) %>%  # replace NA by zero
   mutate(temp = replace(value, value < 5, 0)) %>%
-  group_by(site, year, logger) %>% 
-  mutate(cumTemp = cumsum(temp))
-
-save(climateData, file = "climateData.Rdata")
-
-
-# PLOT DATA
-dailyTemperature %>%
-  filter(site %in% c("Skj", "Lav", "Gud", "Ves", "Ram", "Hog"), logger %in% c("temp30cm")) %>%
-  ggplot(aes(x = date, y = value, color = logger)) +
+  group_by(site) %>% 
+  mutate(cumTemp = cumsum(temp)) %>%
+  ggplot(aes(x = date, y = cumTemp)) +
   geom_line() +
   facet_wrap(~site)
 
-
-
-# calculate cumsum
-dailyTemperature %>%
-  filter(date > (ymd("2014.1.1")), date < (ymd("2015.12.31"))) %>%
-  filter (site %in% c("Skj", "Lav", "Gud", "Ves", "Ram", "Hog")) %>% 
-  filter(logger %in% c("temp30cm", "tempabove")) %>%
-  ggplot(aes(x = date, y = CumTemperature, color = logger)) + 
-  geom_line() +
-  facet_wrap(~site)
-
-dailyTemperature %>%
-  filter(date > (ymd("2014.1.1")), date < (ymd("2015.12.31"))) %>%
-  filter (site %in% c("Skj", "Lav", "Gud", "Ves", "Ram", "Hog")) %>% 
-  filter(logger %in% c("temp30cm", "tempabove")) %>%
-  ggplot(aes(x = date, y = value, color = logger)) + 
-  geom_line() +
-  facet_wrap(~site)
+head(climateData)
