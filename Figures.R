9982aa2931bfccc3f04524e1bfe839e12b705399
audhalbritter <me@audhalbritter.com>
2016-11-20 10:51:59 +0100

    Prepare chunks for figures

--- Figures.R
+++ Figures.R
@@ -1,103 +1,148 @@
 #### FIGURE ABOUT PHENOLOGY
 
-head(Ranunculus)
+## ----loadPhenology
+library("lubridate")
+library("ggplot2")
+library("tidyr")
+library("dplyr")
 library("cowplot")
 
+# load data
+load("Ranunculus.RData")
+
+# set the theme
+th <- theme()
+
+
+## ----FirstTrial
 ### FIGURE ### DA CONTROLLARE
 ### DOY
-ggplot() +
-  geom_boxplot(data = ran, aes(x= so, y = doy.f, fill=trt)) +
+FirstTrial <- Ranunculus %>% 
+  filter(pheno.unit == "doy", pheno.stage == "Flower") %>% 
+  ggplot() +
+  ggtitle("First flowering") +
+  geom_boxplot(aes(x= orig, y = value, fill=trt)) +
   scale_fill_manual(values=c("white", "red", "blue", "purple"))
 
+print(FirstTrial)
+
+## ----FirstTrialDogs
 ### DOGS
 ggplot() +
   geom_boxplot(data = ran, aes(x= so, y = dogs.f, fill=trt)) +
   scale_fill_manual(values=c("white", "red", "blue", "purple"))
 
+## ----SecondTrial
+SecondTrial <- Ranunculus %>%
+  filter(pheno.unit == "dogs") %>% 
+  filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
+  group_by(site, orig, trt, pheno.unit, pheno.stage, OrigTempLevel, OrigPrecLevel) %>% 
+  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
+  mutate(OrigTempLevel2 = plyr::mapvalues(OrigTempLevel, c("1", "2"), c("Alpine", "Subalpine"))) %>%
+  mutate(OrigPrecLevel2 = plyr::mapvalues(OrigPrecLevel, c("1", "2"), c("dry", "wet"))) %>%
+  mutate(grouping = paste(OrigTempLevel2, OrigPrecLevel2, sep = " ")) %>% 
+  ggplot(aes(x = trt, y = mean, shape = pheno.stage, color = pheno.stage, ymax = mean + se, ymin = mean - se)) +
+  geom_point(size = 3) +
+  ylab(paste("Mean value in days after Snowmelt")) + xlab(paste("")) +
+  geom_errorbar(width=0.2) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  facet_grid(~ grouping)
 
+print(SecondTrial)
+
+## ----EffectSizePlot
 ### MAKING FIGURE
 ## PLASTICITY
-### Difference between Control and Treatment
-EffectSize <- Ranunculus %>% 
+### PLOT FOR DIFFERENCE IN TREATMENT - CONTROL IN DOY
+# Get Mean and SE
+MeanSE <- Ranunculus %>% 
   filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
-  filter(pheno.unit == "dogs") %>% 
+  filter(pheno.unit == "doy") %>% # select unit: doy or dogs plot
   group_by(trt, site, orig, pheno.unit, pheno.stage) %>% 
-  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
+  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N))
+
+
+# Calculate difference between Control and Treatment for SE
+SEData <- MeanSE %>% 
   ungroup() %>% 
-  select(-site, -N, -se) %>% 
-  spread(key = trt, value = mean) %>% 
-  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WW = WW - Control) %>% 
-  gather(key = Treatment, value = Effect, -orig, -pheno.unit, -pheno.stage, -Control) %>% 
-  filter(!is.na(Effect)) %>% 
-  mutate(newname = paste(orig, Treatment, sep = "_")) %>% 
-  mutate(newname = plyr::mapvalues(newname, c("GUD_Warmer", "SKJ_Warmer", "GUD_Wetter", "RAM_Wetter", "GUD_WW"), c("dry", "wet", "alpine", "subalpine", "Warm & Wet"))) %>% 
-  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "Warm & Wet")))
+  select(-mean, -N, -site) %>% # remove site, because it causes problems
+  spread(key = trt, value = se) %>% # spread Treatments
+  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
+  gather(key = Treatment, value = SE, -orig, -pheno.unit, -pheno.stage, -Control) %>% # gather Treatments
+  filter(!is.na(SE)) %>% # remove e.g. Warmer in RAM, no such treatment
+  mutate(newname = paste(orig, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
+  mutate(newname = plyr::mapvalues(newname, c("GUD_Warmer", "SKJ_Warmer", "GUD_Wetter", "RAM_Wetter", "GUD_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
+  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
+  select(SE, orig, pheno.stage, Treatment)
+
+# Calculate difference between Control and Treatment for Mean
+MeanData <- MeanSE %>% 
+  ungroup() %>% 
+  select(-se, -N, -site) %>% # remove site, because it causes problems
+  spread(key = trt, value = mean) %>% # spread Treatments
+  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
+  gather(key = Treatment, value = Effect, -orig, -pheno.unit, -pheno.stage, -Control) %>% # gather Treatments
+  filter(!is.na(Effect)) %>% # remove e.g. Warmer in RAM, no such treatment
+  mutate(newname = paste(orig, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
+  mutate(newname = plyr::mapvalues(newname, c("GUD_Warmer", "SKJ_Warmer", "GUD_Wetter", "RAM_Wetter", "GUD_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
+  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
+  left_join(SEData, by = c("orig" = "orig", "pheno.stage" = "pheno.stage", "Treatment" = "Treatment")) %>%  # join SE
+  mutate(Treatment = factor(Treatment, levels = c("Control", "Warmer", "Wetter", "WarmWet"))) 
   
 
-EffectSizePlot <- ggplot(EffectSize, aes(x = newname, y = Effect, color = Treatment)) +
+EffectSizePlot <- ggplot(MeanData, aes(x = newname, y = Effect, color = Treatment, ymax = Effect + SE, ymin = Effect - SE)) +
   geom_point(size = 1.8) +
   labs(x = "", y = "Treatment - control in days") +
   scale_colour_manual(name = "", values = c("red", "blue", "purple")) +
   geom_hline(yintercept=0, color = "gray") +
   facet_grid(~ pheno.stage) +
+  geom_errorbar(width=0.2) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-save_plot("EffectSizePlot_Dogs.jpeg", EffectSizePlot,base_aspect_ratio = 1.8)
+print(EffectSizePlot)
+#save_plot("EffectSizePlot_Doy.jpeg", EffectSizePlot,base_aspect_ratio = 1.8)
 
 
-
-### DURATION
-DurationPlot <- Ranunculus %>% 
+## ----EventPlot
+### PLOT FOR ONSET AND DURATION OF EVENTS
+EventMeanAndSE <- Ranunculus %>% 
   filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
   filter(pheno.unit != "doy") %>% 
-  mutate(pheno.stage = plyr::mapvalues(pheno.stage, c("Bud", "Flower", "Fruit""SM-Bud", "Bud-Flower", "Flower-Fruit"), c("B", "Fl", "Fr", "SMB", "BFl", "FlFr"))) %>%
-  mutate(pheno.stage = factor(pheno.stage, levels = c("B", "Fl", "Fr", "SMB", "BFl", "FlFr"))) %>%
-  group_by(trt, site, orig, pheno.stage) %>% 
-  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
-  ggplot(aes(x = pheno.stage, y = mean, color = trt, group = trt, ymax = mean + se, ymin = mean - se)) +
-  scale_colour_manual(name = "Treatment", values = c("grey", "red", "blue", "purple")) +
-  labs(x = "", y = "Duration between events in days") +
-  geom_point() +
-  geom_line() +
-  geom_errorbar(width=0.2) +
-  facet_grid(~ orig)
-save_plot("DurationPlot.jpeg", DurationPlot, base_aspect_ratio = 1.8)
-
-
-DurationPlot <- Ranunculus %>% 
-  filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
-  filter(pheno.unit != "doy") %>% 
-  mutate(pheno.stage = plyr::mapvalues(pheno.stage, c("Bud", "Flower", "Fruit", "SM-Bud", "Bud-Flower", "Flower-Fruit"), c("B", "Fl", "Fr", "B", "Fl", "Fr"))) %>%
-  mutate(pheno.stage = factor(pheno.stage, levels = c("B", "Fl", "Fr", "B", "Fl", "Fr"))) %>%
+  mutate(pheno.stage = plyr::mapvalues(pheno.stage, c("Bud", "Flower", "Fruit", "SM-Bud", "Bud-Flower", "Flower-Fruit"), c("Bud", "Flower", "Fruit", "Bud", "Flower", "Fruit"))) %>%
+  mutate(pheno.stage = factor(pheno.stage, levels = c("Bud", "Flower", "Fruit", "Bud", "Flower", "Fruit"))) %>%
   mutate(orig = plyr::mapvalues(orig, c("GUD", "RAM", "SKJ"), c("Alpine-dry", "Subalpine-dry", "Alpine-wet"))) %>%
   mutate(orig = factor(orig, levels = c("Alpine-dry", "Alpine-wet", "Subalpine-dry"))) %>%
   group_by(trt, site, orig, pheno.stage, pheno.unit) %>% 
-  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N))
+  
+EventSE <- EventMeanAndSE %>% 
+  select(-mean, -N) %>% # remove mean
+  spread(key = pheno.unit, value = se)
+  
+EventData <- EventMeanAndSE %>% 
+  select(-se, -N) %>% # remove mean
   spread(key = pheno.unit, value = mean) %>% 
-  ggplot(aes(x = dogs, y = days, shape = pheno.stage, color = trt, group = trt)) +
+  left_join(EventSE, by = c("orig" = "orig", "site" = "site", "pheno.stage" = "pheno.stage", "trt" = "trt"),  suffix = c(".mean", ".se"))  # join SE
+  
+
+OnsetDurationEventPlot <- ggplot(EventData, aes(x = dogs.x, y = days.x, shape = pheno.stage, color = trt, group = trt, ymax = days.x + days.y, ymin = days.x - days.y, xmax = dogs.x + dogs.y, xmin = dogs.x - dogs.y)) +
   scale_colour_manual(name = "Treatment", values = c("grey", "red", "blue", "purple")) +
   scale_shape_manual(name = "Event", values = c(16,17,15)) +
-  labs(x = "Onset of event after SM", y = "Duration between events in days") +
+  labs(x = "Onset of event in days after Snowmelt", y = "Duration between events in days") +
+  geom_errorbar(width=0.2) +
+  geom_errorbarh() +
   geom_line(linetype="dashed") +
   geom_point() +
-  #geom_errorbar(width=0.2) +
   facet_grid(~ orig)
-  
-save_plot("DurationPlotDOGS.jpeg", DurationPlot, base_aspect_ratio = 2)
+ 
+print(OnsetDurationEventPlot) 
+#save_plot("OnsetDurationEventPlotDOGS.jpeg", OnsetDurationEventPlot, base_aspect_ratio = 2)
 
 
 
-Ranunculus %>%
-  filter(pheno.unit == "days") %>% 
-  filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
-  group_by(trt, pheno.stage) %>% 
-  summarise(N = sum(!is.na(value)), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
-  ggplot(aes(x = trt, y = mean, shape = pheno.stage, color = pheno.stage, ymax = mean + se, ymin = mean - se)) +
-  geom_point(size = 3) +
-  ylab(paste("Mean value in days after SM")) + xlab(paste("")) +
-  geom_errorbar(width=0.2)
 
 
+## ----Rest
 ### Plasticity Figure
 Ranunculus %>%
   filter(pheno.unit == "dogs") %>% 
