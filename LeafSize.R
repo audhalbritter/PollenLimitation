4564c8612630b995d7807c8d9aea37f37d28cf61
audhalbritter <me@audhalbritter.com>
2017-06-26 09:49:27 +0200

    Plot on leaf size

--- LeafSize.R
+++ LeafSize.R
@@ -4,7 +4,7 @@ head(size)
 size <- size %>% 
   filter(sp == "RAN") %>% 
   select(-Torig, -Porig, -Tdest, -Pdest, -DOY, -blk) %>% 
-  mutate(trt = plyr::mapvalues(trt, c("c", "wa", "we", "ww"), c("Control", "Warmer", "Wetter", "WarmWet")))
+  mutate(trt = plyr::mapvalues(trt, c("c", "wa", "we", "ww"), c("Control", "Warmer", "LaterSM", "WarmLate")))
 
 size %>% 
   ggplot() +
@@ -13,10 +13,10 @@ size %>%
   scale_fill_manual(values=c("white", "red", "blue", "purple"))
 
 
-MeanSE <- size %>% 
+SizeMeanSE <- size %>% 
   filter(orig != "VES" | trt != "Control") %>% # remove Control at Veskre
   group_by(trt, site, orig) %>% 
-  summarise(N = sum(!is.na(size_end)), mean = mean(size_end, na.rm = TRUE), se = 2*sd(size_end, na.rm = TRUE)/sqrt(N))
+  summarise(N = sum(!is.na(size_end)), mean = mean(size_end, na.rm = TRUE), se = sd(size_end, na.rm = TRUE)/sqrt(N))
 
 # only for checking results
 MeanSE %>% filter(trt %in% c("Control", "Warmer")) %>% 
@@ -24,79 +24,81 @@ MeanSE %>% filter(trt %in% c("Control", "Warmer")) %>%
   geom_point() +
   geom_line()
 
+SizeMeanSE %>% 
+  ggplot(aes(x = site, y = mean, color = trt)) + 
+  geom_point() +
+  facet_grid(~ orig)
+
 # Calculate difference between Control and Treatment for SE
-SEData <- MeanSE %>% 
+SizeSEData <- SizeMeanSE %>% 
   ungroup() %>% 
   select(-mean, -N, -site) %>% # remove site, because it causes problems
   spread(key = trt, value = se) %>% # spread Treatments
-  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
-  gather(key = Treatment, value = SE, -orig, -Control) %>% # gather Treatments
+  mutate(Warmer = sqrt(Control^2 + Warmer^2), LaterSM = sqrt(Control^2 + LaterSM^2), WarmLate = sqrt(Control^2 + WarmLate^2)) %>%
+  select(-Control) %>% 
+  gather(key = Treatment, value = SE, -orig) %>% # gather Treatments
   filter(!is.na(SE)) %>% # remove e.g. Warmer in RAM, no such treatment
-  mutate(newname = paste(orig, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
-  mutate(newname = plyr::mapvalues(newname, c("GUD_Warmer", "SKJ_Warmer", "GUD_Wetter", "RAM_Wetter", "GUD_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
   select(SE, orig, Treatment)
+  
 
 # Calculate difference between Control and Treatment for Mean
-MeanData <- MeanSE %>% 
+SizeMeanData <- SizeMeanSE %>% 
   ungroup() %>% 
   select(-se, -N, -site) %>% # remove site, because it causes problems
   spread(key = trt, value = mean) %>% # spread Treatments
-  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
-  gather(key = Treatment, value = Effect, -orig, -Control) %>% # gather Treatments
+  mutate(Warmer = Warmer - Control, LaterSM = LaterSM - Control, WarmLate = WarmLate - Control) %>% # Difference Treatment - Control
+  #mutate(Warmer.prop = Warmer * 100 / Control, LaterSM.prop = LaterSM * 100 / Control, WarmLate.prop = WarmLate * 100 / Control) %>% # Difference Treatment - Control
+  select(-Control) %>%
+  gather(key = Treatment, value = Effect, -orig) %>% # gather Treatments
   filter(!is.na(Effect)) %>% # remove e.g. Warmer in RAM, no such treatment
-  mutate(newname = paste(orig, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
-  mutate(newname = plyr::mapvalues(newname, c("GUD_Warmer", "SKJ_Warmer", "GUD_Wetter", "RAM_Wetter", "GUD_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  left_join(SEData, by = c("orig" = "orig", "Treatment" = "Treatment")) %>%  # join SE
-  mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "Wetter", "WarmWet"), c("Warmer", "Wetter", "Warm & wet"))) %>%
-  mutate(Treatment = factor(Treatment, levels = c("Warmer", "Wetter", "Warm & wet"))) 
+  left_join(SizeSEData, by = c("orig" = "orig", "Treatment" = "Treatment")) %>% 
+  left_join(SizeMeanSE, by = c("orig" = "orig", "Treatment" = "trt")) %>% 
+  select(-mean, -se) %>% 
+  mutate(orig = plyr::mapvalues(orig, c("GUD", "RAM", "SKJ"), c("Alpine-early", "Subalpine-early", "Alpine-late"))) %>%
+  mutate(pheno.stage = "Leaf length") %>%
+  mutate(pheno.unit = "cm") %>%
+  rename(mean = Effect, se = SE) %>%
+  mutate(newname = NA) %>% 
+  left_join(SMDiff, by = c("orig", "Treatment", "site")) %>% 
+  select(orig, pheno.stage, pheno.unit, Treatment, mean, se, newname, N, site, sm, smOrig, smDiff)
+  
 
 
-LeafSizePlot <- ggplot(MeanData, aes(x = newname, y = Effect, color = Treatment, ymax = Effect + SE, ymin = Effect - SE)) +
-  geom_hline(yintercept=0, color = "gray") +
-  geom_point(size = 1.8) +
-  labs(x = "", y = "Treatment - origin\n control for leaf size in cm") +
+LeafSizePlot <- SizeMeanData %>% 
+  mutate(Treatment = factor(Treatment, levels = c("Warmer", "LaterSM", "WarmLate"))) %>% 
+  ggplot(aes(x = smDiff, y = mean, color = Treatment, ymax = mean + se, ymin = mean - se)) +
+  geom_hline(yintercept=0, color = "gray", linetype = "dashed") +
+  geom_point(size = 3) +
+  labs(y = "Difference in leaf size [cm] between\n treatment and origin-control", x = "Difference in SMT between\n origin and destination site [days]", title = "Phenotypic plasticity: origin-control") +
   scale_colour_manual(name = "Treatment", values = c("red", "blue", "purple")) +
   ylim(-2.5, 3) +
   geom_errorbar(width=0.2) +
-  theme(text = element_text(size = 9), axis.text = element_text(size = 9))
+  panel_border(colour = "black", remove = FALSE) +
+  annotate(geom = "text", x = 32, y = 3, label = "larger", color = "grey20") +
+  annotate(geom = "text", x = 32, y = -2.5, label = "smaller", color = "grey20") +
+  theme(legend.position="none")
 
-#print(LeafSizePlot)
-save_plot("LeafSizePlot_PP.jpeg", LeafSizePlot,base_aspect_ratio = 1.2)
 
 
 
-#### ADAPTATION
-
-size %>% 
-  ggplot() +
-  geom_boxplot(aes(x= site, y = size_end, fill=trt)) +
-  scale_fill_manual(values=c("white", "red", "blue", "purple"))
+########################################################################
+#### ADAPTATION #####
+########################################################################
 
 MeanSEAdapt <- size %>% 
   filter(orig != "GUD" | trt != "Control") %>% # remove Control at Gudmedalen
   group_by(trt, site, orig) %>% 
   summarise(N = sum(!is.na(size_end)), mean = mean(size_end, na.rm = TRUE), se = 2*sd(size_end, na.rm = TRUE)/sqrt(N))
 
-# only for checking results
-MeanSEAdapt %>% filter(trt %in% c("Control", "Wetter")) %>% 
-  ggplot(aes(x = trt, y = mean, color = site, group = site)) +
-  geom_point() +
-  geom_line()
-
-
 # Calculate difference between Control and Treatment for SE
-SEData <- MeanSEAdapt %>% 
+SEAdaptData <- MeanSEAdapt %>% 
   ungroup() %>% 
   select(-mean, -N, -orig) %>% # remove origin, because it causes problems
   spread(key = trt, value = se) %>% # spread Treatments
-  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
-  gather(key = Treatment, value = SE, -site, -Control) %>% # gather Treatments
+  mutate(Warmer = sqrt(Control^2 + Warmer^2), LaterSM = sqrt(Control^2 + LaterSM^2), WarmLate = sqrt(Control^2 + WarmLate^2)) %>%
+  select(-Control) %>% 
+  gather(key = Treatment, value = SE, -site) %>% # gather Treatments
   filter(!is.na(SE)) %>% # remove e.g. Warmer in RAM, no such treatment
-  mutate(newname = paste(site, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
-  mutate(newname = plyr::mapvalues(newname, c("RAM_Warmer", "VES_Warmer", "SKJ_Wetter", "VES_Wetter", "VES_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
   select(SE, site, Treatment)
 
 # Calculate difference between Control and Treatment for Mean
@@ -104,30 +106,38 @@ MeanDataAdapt <- MeanSEAdapt %>%
   ungroup() %>% 
   select(-se, -N, -orig) %>% # remove orig, because it causes problems
   spread(key = trt, value = mean) %>% # spread Treatments
-  mutate(Warmer = Warmer - Control, Wetter = Wetter - Control, WarmWet = WarmWet - Control) %>% # Difference Treatment - Control
-  gather(key = Treatment, value = Effect, -site, -Control) %>% # gather Treatments
+  mutate(Warmer = Warmer - Control, LaterSM = LaterSM - Control, WarmLate = WarmLate - Control) %>% # Difference Treatment - Control
+  select(-Control) %>% 
+  gather(key = Treatment, value = Effect, -site) %>% # gather Treatments
   filter(!is.na(Effect)) %>% # remove e.g. Warmer in RAM, no such treatment
-  mutate(newname = paste(site, Treatment, sep = "_")) %>% # paste Treatment and site, they are unique and can be renamed
-  mutate(newname = plyr::mapvalues(newname, c("RAM_Warmer", "VES_Warmer", "SKJ_Wetter", "VES_Wetter", "VES_WarmWet"), c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  mutate(newname = factor(newname, levels = c("dry", "wet", "alpine", "subalpine", "warm & wet"))) %>% 
-  left_join(SEData, by = c("site" = "site", "Treatment" = "Treatment")) %>%  # join SE
-  mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "Wetter", "WarmWet"), c("Warmer", "Wetter", "Warm & wet"))) %>% 
-  mutate(Treatment = factor(Treatment, levels = c("Warmer", "Wetter", "Warm & wet"))) 
+  left_join(SEAdaptData, by = c("site" = "site", "Treatment" = "Treatment")) %>%  # join SE
+  left_join(MeanSEAdapt, by = c("site" = "site", "Treatment" = "trt")) %>% 
+  select(-mean, -se) %>% 
+  mutate(site = plyr::mapvalues(site, c("RAM", "VES", "SKJ"), c("Subalpine-early", "Subalpine-late", "Alpine-late"))) %>% 
+  mutate(pheno.stage = "Leaf length") %>%
+  mutate(pheno.unit = "cm") %>%
+  rename(mean = Effect, se = SE) %>%
+  mutate(newname = NA) %>% 
+  left_join(SMDiffAdapt, by = c("orig", "Treatment", "site")) %>% 
+  select(orig, pheno.stage, pheno.unit, Treatment, mean, se, newname, N, site, sm, smOrig, smDiff) %>% 
+  mutate(Treatment = factor(Treatment, levels = c("Warmer", "LaterSM", "WarmLate"))) 
 
-
-LeafSizePlotAdapt <- ggplot(MeanDataAdapt, aes(x = newname, y = Effect, color = Treatment, ymax = Effect + SE, ymin = Effect - SE)) +
-  geom_hline(yintercept=0, color = "gray") +
-  geom_point(size = 1.8) +
-  labs(x = "", y = "Treatment - dest.\n control for leaf size in cm") +
-  scale_colour_manual(name = "", values = c("red", "blue", "purple")) +
+  
+LeafSizePlotAdapt <- MeanDataAdapt %>% 
+  mutate(Pvalue = factor(1)) %>% 
+  ggplot(aes(x = smDiff, y = mean, color = Treatment, shape = Pvalue, ymax = mean + se, ymin = mean - se)) +
+  geom_hline(yintercept=0, color = "gray", linetype = "dashed") +
+  geom_point(size = 3) +
+  labs(y = "Difference in leaf size [cm] between\n treatment and destination-control", x = "Difference in SMT between\n origin and destination site [days]", title = "Genetic differentiation: destination-control") +
+  scale_colour_manual(values = c("red", "blue", "purple")) +
+  scale_shape_manual(values = c(1)) +
   ylim(-2.5, 3) +
   geom_errorbar(width=0.2) +
-  theme(text = element_text(size = 9), axis.text = element_text(size = 9))
+  panel_border(colour = "black", remove = FALSE) +
+  annotate(geom = "text", x = 32, y = 3, label = "larger", color = "grey20") +
+  annotate(geom = "text", x = 32, y = -2.5, label = "smaller", color = "grey20") +
+  theme(legend.position="none")
 
 
-#print(LeafSizePlotAdapt)
-save_plot("LeafSizePlotAdapt.jpeg", LeafSizePlotAdapt,base_aspect_ratio = 1.2)
-
-library("cowplot")
-LeafSizes <- plot_grid(LeafSizePlot, LeafSizePlotAdapt, labels = c("1)", "2)"), nrow = 2, align = "v")
-save_plot("LeafSizes.jpeg", LeafSizes,base_aspect_ratio = 1.3)
+LeafSizes <- plot_grid(LeafSizePlot, LeafSizePlotAdapt, nrow = 1, align = "h")
+ggsave(LeafSizes, filename = "LeafSizes.pdf")
