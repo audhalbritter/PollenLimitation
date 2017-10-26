a534863d3e9a1c27f0ab68bfcd02d9a2e5878518
Deborah Davy <EnterYourEmailAddress>
2017-10-26 20:08:50 +0200

    These were fine

--- EasyPlots.R
+++ EasyPlots.R
@@ -39,8 +39,16 @@ Pollination %>%
   facet_wrap(~ Origin)
 
 Pollination %>% 
-  filter(Year == "2017", pheno.stage == "RepOutput", Species == "RAN") %>% 
+  filter(Year == "2017", pheno.stage == "Flower", Species == "RAN") %>% 
   ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
   geom_boxplot() +
   scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
   facet_wrap(~ Origin)
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "Flower", Species == "RAN") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
+
