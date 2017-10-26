1531e34fc781839c5bcd6115e442ae5f4a12a2a3
Deborah Davy <EnterYourEmailAddress>
2017-10-26 20:08:16 +0200

    Got Stuck

--- EasyPlots.R
+++ EasyPlots.R
@@ -6,3 +6,41 @@ Pollination %>%
   geom_boxplot() +
   scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
   facet_wrap(~ Origin)
+
+
+
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "Bud", Species == "RAN") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "Flower", Species == "LEO") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "Seed", Species == "RAN") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "Flower", Species == "LEO") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
+
+Pollination %>% 
+  filter(Year == "2017", pheno.stage == "RepOutput", Species == "RAN") %>% 
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot() +
+  scale_fill_manual(values=c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ Origin)
