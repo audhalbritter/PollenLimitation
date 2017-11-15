950cbb3048badb70fd99fc9c4e904de27c6e633b
audhalbritter <audhalbritter.com>
2017-11-15 16:41:19 +0100

    Add plots for biomass

--- DiffPlot2.R
+++ DiffPlot2.R
@@ -1,6 +1,6 @@
-# ORIGIN - PHENOTYPIC PLASTICITY
+#### ORIGIN - PHENOTYPIC PLASTICITY ####
 
-# Snowmelt difference
+### SNWOMELT ###
 SMDiff <- Pollination %>% 
   select(Origin, Treatment, Year, Species, SM) %>% 
   distinct(Origin, Treatment, Year, Species, SM) %>% 
@@ -11,6 +11,7 @@ SMDiff <- Pollination %>%
   mutate(Origin = plyr::mapvalues(Origin, c("GUD", "RAM", "SKJ"), c("Alpine-early", "Subalpine-early", "Alpine-late")))
 
 
+### PHENOLOGY ###
 EventDiffData <- Pollination %>% 
   filter(Origin != "VES" | Treatment != "Control") %>% # remove Control at Veskre
   mutate(Origin = plyr::mapvalues(Origin, c("GUD", "RAM", "SKJ"), c("Alpine-early", "Subalpine-early", "Alpine-late"))) %>% 
@@ -43,38 +44,88 @@ EventDiff <- EventDiffData %>%
   filter(!is.na(mean))
 
 
-EventDiff %>% 
-  filter(Species == "RAN", pheno.stage == "Flower") %>% 
+EventPlastic <- EventDiff %>% 
+  filter(Year == 2017, pheno.stage != "Ripe Seed") %>% 
   left_join(SMDiff, by = c("Species", "Year", "Origin", "Treatment")) %>% 
   mutate(pheno.stage = factor(pheno.stage, levels = c("Bud", "Flower", "Seed"))) %>%
   mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "LaterSM", "WarmLate"), c("Warmer", "Later SM", "Warm & late SM"))) %>%
   mutate(Treatment = factor(Treatment, levels = c("Warmer", "Later SM", "Warm & late SM"))) %>%
-  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, shape = factor(Year), alpha = factor(Year), ymax = mean + 1.96*se, ymin = mean - 1.96*se, linetype = Origin)) +
+  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, ymax = mean + 1.96*se, ymin = mean - 1.96*se)) + #linetype = Origin
   geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
   scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
-  scale_shape_manual(name = "Treatment:", values = c(16, 17)) +
-  scale_alpha_manual(name = "Treatment:", values = c(1, 0.3)) +
-  scale_linetype_manual(values = c("solid", "solid", "dashed")) +
+  #scale_shape_manual(name = "Treatment:", values = c(16, 17)) +
+  #scale_alpha_manual(name = "Treatment:", values = c(1, 0.3)) +
+  #scale_linetype_manual(values = c("solid", "solid", "dashed")) +
   #scale_fill_manual(name = "Year:", values = c("white", "red")) +
-  labs(y = "", x = "", title = "") +
-  geom_errorbar(width=0.18) +
-  geom_point(size = 4)
-
-#+
-#theme(legend.position="none") +
-panel_border(colour = "black", remove = FALSE) +
+  labs(y = "Difference in onset of stage [days] after SMT \n between treatment and origin-control", x = "Difference in SMT between origin and destination site [days]", title = "Phenotypic plasticity: origin-control") +
+  geom_errorbar(width = 0.18) +
+  geom_point(size = 4) +
+  panel_border(colour = "black", remove = FALSE) +
   annotate(geom = "text", x = 25, y = 13, label = "later", color = "grey20") +
   annotate(geom = "text", x = 25, y = -25, label = "earlier", color = "grey20") +
-  theme(legend.position = "none")
-#+ facet_grid( ~ pheno.stage)
-ggsave(YearPlot, filename = "YearPlot.pdf", height = 4.5, width = 4.5)
+  theme(legend.position = "none") +
+  facet_grid(Species ~ pheno.stage)
+ggsave(EventPlastic, filename = "EventPlastic.pdf", height = 6, width = 8)
 
 
 
+### BIOMASS ###
+BiomassDiffData <- Pollination %>% 
+  filter(Origin != "VES" | Treatment != "Control", pheno.stage == "Bud") %>% # remove Control at Veskre
+  mutate(Origin = plyr::mapvalues(Origin, c("GUD", "RAM", "SKJ"), c("Alpine-early", "Subalpine-early", "Alpine-late"))) %>% 
+  mutate(Origin = factor(Origin, levels = c("Alpine-early", "Alpine-late", "Subalpine-early"))) %>%
+  group_by(Species, Year, Treatment, Site, Origin) %>% 
+  summarise(N = sum(!is.na(EndSize)), mean = mean(EndSize, na.rm = TRUE), se = sd(EndSize, na.rm = TRUE)/sqrt(N))
 
-# DESTINATION - ADAPTATION
 
-# Snowmelt difference
+BiomassDiff <- BiomassDiffData %>% 
+  ungroup(Site) %>% 
+  select(-Site,-N) %>% 
+  unite(united, mean, se, sep = "_") %>% 
+  spread(key = Treatment, value = united) %>% 
+  separate(col = Control, into = c("Control_mean", "Control_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = Warmer, into = c("Warmer_mean", "Warmer_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = LaterSM, into = c("LaterSM_mean", "LaterSM_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = WarmLate, into = c("WarmLate_mean", "WarmLate_se"), sep = "_", convert = TRUE) %>% 
+  # calculate difference between control and treatment in mean
+  mutate(Warmer_mean = Warmer_mean - Control_mean, LaterSM_mean = LaterSM_mean - Control_mean, WarmLate_mean = WarmLate_mean - Control_mean) %>% 
+  # calculate SE for difference
+  mutate(Warmer_se = sqrt(Control_se^2 + Warmer_se^2), LaterSM_se = sqrt(Control_se^2 + LaterSM_se^2), WarmLate_se = sqrt(Control_se^2 + WarmLate_se^2)) %>% 
+  select(-Control_mean, -Control_se) %>% 
+  unite(Warmer, Warmer_mean, Warmer_se, sep = "_") %>% 
+  unite(LaterSM, LaterSM_mean, LaterSM_se, sep = "_") %>% 
+  unite(WarmLate, WarmLate_mean, WarmLate_se, sep = "_") %>% 
+  gather(key = Treatment, value = united, -Species, -Year, -Origin) %>% 
+  separate(col = united, into = c("mean", "se"), sep = "_", convert = TRUE) %>% 
+  #mutate(mean = replace(mean, c(pheno.stage == "Fruit" & Treatment == "LaterSM"), NA)) %>% 
+  #mutate(se = replace(se, c(pheno.stage == "Fruit" & Treatment == "LaterSM"), NA)) %>% 
+  filter(!is.na(mean))
+
+
+BiomassPlastic <- BiomassDiff %>% 
+  left_join(SMDiff, by = c("Species", "Year", "Origin", "Treatment")) %>% 
+  mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "LaterSM", "WarmLate"), c("Warmer", "Later SM", "Warm & late SM"))) %>%
+  mutate(Treatment = factor(Treatment, levels = c("Warmer", "Later SM", "Warm & late SM"))) %>%
+  filter(Year == 2017) %>% 
+  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, ymax = mean + 1.96*se, ymin = mean - 1.96*se)) + #linetype = Origin
+  geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
+  scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
+  #scale_shape_manual(name = "Flowering:", values = c(1, 16)) +
+  labs(y = "Difference in biomass [g] \n between treatment and origin-control", x = "Difference in SMT between origin and destination site [days]", title = "Phenotypic plasticity: origin-control") +
+  geom_errorbar(width = 0.18) +
+  geom_point(size = 4) +
+  panel_border(colour = "black") +
+  #annotate(geom = "text", x = 25, y = 13, label = "higher", color = "grey20") +
+  #annotate(geom = "text", x = 25, y = -25, label = "lower", color = "grey20") +
+  facet_grid( ~ Species)
+ggsave(BiomassPlastic, filename = "BiomassPlastic.pdf", height = 4, width = 8)
+
+
+
+###********************************************************************************************
+#### DESTINATION - ADAPTATION ####
+
+### SNOWMELT ###
 SMDiffAdapt <- Pollination %>% 
   select(Site, Origin, Treatment, Year, Species, SM) %>% 
   distinct(Site, Origin, Treatment, Year, Species, SM) %>% 
@@ -86,6 +137,7 @@ SMDiffAdapt <- Pollination %>%
 
 
 
+### PHENOLOGY ###
 EventDiffAdaptData <- Pollination %>% 
   filter(Origin != "GUD" | Treatment != "Control") %>% # remove Control at Gudmedalen
   mutate(Site = plyr::mapvalues(Site, c("RAM", "VES", "SKJ"), c("Subalpine-early", "Subalpine-late", "Alpine-late"))) %>%
@@ -119,23 +171,79 @@ EventDiffAdapt <- EventDiffAdaptData %>%
 
 
 
-EventDiffAdapt %>% 
-  filter(Species == "RAN", pheno.stage == "Bud") %>% 
+EventAdapt <- EventDiffAdapt %>% 
+  filter(Year == 2017, pheno.stage != "Ripe Seed") %>% 
   left_join(SMDiffAdapt, by = c("Species", "Year", "Site", "Treatment")) %>% 
   mutate(pheno.stage = factor(pheno.stage, levels = c("Bud", "Flower", "Seed"))) %>%
   mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "LaterSM", "WarmLate"), c("Warmer", "Later SM", "Warm & late SM"))) %>%
   mutate(Treatment = factor(Treatment, levels = c("Warmer", "Later SM", "Warm & late SM"))) %>%
-  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, shape = factor(Year), alpha = factor(Year), ymax = mean + 1.96*se, ymin = mean - 1.96*se)) +
+  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, ymax = mean + 1.96*se, ymin = mean - 1.96*se)) +
   geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
   scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
-  scale_shape_manual(name = "Treatment:", values = c(16, 17)) +
-  scale_alpha_manual(name = "Treatment:", values = c(1, 0.3)) +
+  #scale_shape_manual(name = "Treatment:", values = c(16, 17)) +
+  #scale_alpha_manual(name = "Treatment:", values = c(1, 0.3)) +
   #scale_fill_manual(name = "Year:", values = c("white", "red")) +
-  labs(y = "", x = "", title = "") +
+  labs(y = "Difference in onset of stage [days] after SMT \n between treatment and destination-control", x = "Difference in SMT between origin and destination site [days]", title = "Genetic difference: destination-control") +
   geom_errorbar(width=0.18) +
   geom_point(size = 3) +
-  #theme(legend.position="none") +
   panel_border(colour = "black", remove = FALSE) +
   annotate(geom = "text", x = 25, y = 13, label = "later", color = "grey20") +
   annotate(geom = "text", x = 25, y = -25, label = "earlier", color = "grey20") +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  facet_grid(Species ~ pheno.stage)
+ggsave(EventAdapt, filename = "EventAdapt.pdf", height = 6, width = 8)
+
+
+
+### BIOMASS ###
+BiomassDiffDataAdapt <- Pollination %>% 
+  filter(Origin != "GUD" | Treatment != "Control", pheno.stage == "Bud") %>% # remove Control at Gudmedalen
+  mutate(Site = plyr::mapvalues(Site, c("RAM", "VES", "SKJ"), c("Subalpine-early", "Subalpine-late", "Alpine-late"))) %>%
+  mutate(Site = factor(Site, levels = c("Subalpine-early", "Subalpine-late", "Alpine-late"))) %>%
+  group_by(Species, Year, Treatment, Site, Origin) %>% 
+  summarise(N = sum(!is.na(EndSize)), mean = mean(EndSize, na.rm = TRUE), se = sd(EndSize, na.rm = TRUE)/sqrt(N))
+
+
+
+BiomassDiffAdapt <- BiomassDiffDataAdapt %>% 
+  ungroup(Origin) %>% 
+  select(-Origin,-N) %>% 
+  unite(united, mean, se, sep = "_") %>% 
+  spread(key = Treatment, value = united) %>% 
+  separate(col = Control, into = c("Control_mean", "Control_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = Warmer, into = c("Warmer_mean", "Warmer_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = LaterSM, into = c("LaterSM_mean", "LaterSM_se"), sep = "_", convert = TRUE) %>% 
+  separate(col = WarmLate, into = c("WarmLate_mean", "WarmLate_se"), sep = "_", convert = TRUE) %>% 
+  # calculate difference between control and treatment in mean
+  mutate(Warmer_mean = Warmer_mean - Control_mean, LaterSM_mean = LaterSM_mean - Control_mean, WarmLate_mean = WarmLate_mean - Control_mean) %>% 
+  # calculate SE for difference
+  mutate(Warmer_se = sqrt(Control_se^2 + Warmer_se^2), LaterSM_se = sqrt(Control_se^2 + LaterSM_se^2), WarmLate_se = sqrt(Control_se^2 + WarmLate_se^2)) %>% 
+  select(-Control_mean, -Control_se) %>% 
+  unite(Warmer, Warmer_mean, Warmer_se, sep = "_") %>% 
+  unite(LaterSM, LaterSM_mean, LaterSM_se, sep = "_") %>% 
+  unite(WarmLate, WarmLate_mean, WarmLate_se, sep = "_") %>% 
+  gather(key = Treatment, value = united, -Species, -Year, -Site) %>% 
+  separate(col = united, into = c("mean", "se"), sep = "_", convert = TRUE) %>% 
+  #mutate(mean = replace(mean, c(pheno.stage == "Fruit" & Treatment == "LaterSM"), NA)) %>% 
+  #mutate(se = replace(se, c(pheno.stage == "Fruit" & Treatment == "LaterSM"), NA)) %>% 
+  filter(!is.na(mean))
+
+
+BiomassAdapt <- BiomassDiffAdapt %>% 
+  left_join(SMDiffAdapt, by = c("Species", "Year", "Site", "Treatment")) %>% 
+  mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "LaterSM", "WarmLate"), c("Warmer", "Later SM", "Warm & late SM"))) %>%
+  mutate(Treatment = factor(Treatment, levels = c("Warmer", "Later SM", "Warm & late SM"))) %>%
+  filter(Year == 2017) %>% 
+  ggplot(aes(x = SMDiff, y = mean, colour = Treatment, ymax = mean + 1.96*se, ymin = mean - 1.96*se)) + #linetype = Origin
+  geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
+  scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
+  #scale_shape_manual(name = "Flowering:", values = c(1, 16)) +
+  labs(y = "Difference in biomass [g] \n between treatment and destination-control", x = "Difference in SMT between origin and destination site [days]", title = "Genetic differentiation: destination-control") +
+  geom_errorbar(width = 0.18) +
+  geom_point(size = 4) +
+  panel_border(colour = "black") +
+  #annotate(geom = "text", x = 25, y = 13, label = "higher", color = "grey20") +
+  #annotate(geom = "text", x = 25, y = -25, label = "lower", color = "grey20") +
+  facet_grid( ~ Species)
+ggsave(BiomassAdapt, filename = "BiomassAdapt.pdf", height = 4, width = 8)
+
