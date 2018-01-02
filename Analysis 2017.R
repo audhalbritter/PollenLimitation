501fc16cc6c4ad747e9a8b262d2ebe9489318cff
audhalbritter <audhalbritter.com>
2018-01-02 20:16:12 +0100

    automate glm for both species and all pheno stages. And use tidy and glance to print tidy output

--- Analysis 2017.R
+++ Analysis 2017.R
@@ -3,21 +3,35 @@
 # load the data
 source("Mergin 2015 and 2017 data.R")
 
+library("broom")
+
 #***************************************************************************************
 #### PLASTICITY ####
-## WARMER - Flowering date for Leontodon ##
-dat <- Pollination %>% 
-  filter(Species == "LEO", Variable == "Flower") %>% 
+## WARMER - Bud, Flower, Seed and Ripe Seed date for both species ##
+# fit simple glm
+dfPolli <- Pollination %>% 
+  #filter(Species == "LEO", Variable == "Flower") %>% 
   # remove Control plants at Veskre, because they are not needed for the plasticity question
   filter(Origin != c("VES") | Treatment != "Control") %>%  
   filter(Origin != c("RAM") | Treatment != "Control") %>%  
   # select only controls and warmer
   filter(Treatment %in% c("Control", "Warmer")) %>% 
-  filter(!is.na(value)) # remove NAs
+  filter(!is.na(value)) %>%  # remove NAs
+  filter(Variable %in% c("Bud", "Flower", "Seed", "Ripe Seed")) %>% 
+  group_by(Species, Variable) %>% 
+  do(fit = glm(value ~ Treatment * OrigPLevel, data = ., family = "poisson"))
   
-# fit dimple glm
-fit <- glm(value ~ Treatment * OrigPLevel, data = dat, family = "poisson")
-summary(fit)
+# get the coefficients by group in a tidy data_frame
+tidy(dfPolli, fit) %>% 
+  mutate(estimate = round(estimate, 2), std.error = round(std.error, 2), statistic = round(statistic, 2)) %>% 
+  mutate(p.value = round(p.value, 3)) %>% pn
+
+# get the summary statistics by group in a tidy data_frame
+glance(dfPolli, fit) %>% 
+  # check for overdispersion: ratio of residual deviance to degrees of freedom, overdispersion if ratio > 1
+  mutate(ratio = deviance / df.residual)
+
+
 ### IMPORTANT ###
 # - you need to check if model assumptions are met for each of these models
 # - because these are poisson model, you need to check if they are over disperesd.
