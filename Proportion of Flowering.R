65371a7551fcd8100f62d2968f6aca802ff2c28e
audhalbritter <audhalbritter.com>
2019-02-20 20:11:58 +0100

    add model for 2015 data

--- Proportion of Flowering.R
+++ Proportion of Flowering.R
@@ -1,36 +1,36 @@
+##################################################################
+#### RELASHINSHIP BETWEEN GROWTH AND PROBABILITY OF FLOWERING ####
+##################################################################
 source("Merging 2015 and 2017 data.R")
 
 library("lme4")
 library("broom")
 
-Pollination17 <- Pollination %>% 
-  filter(Year == 2017) %>% 
-  # remove Second Flowers
-  filter(!Pollination == "")
-
-#### CORRELATION BETWEEN GROWTH AND PROBABILITY OF FLOWERING ####
-# Is plant size and nr of flowers correlated
-# Only control plants
+# Select data and           
 # calculate Probability of flowering
-ProbFlower <- Pollination17 %>% 
-  filter(Pollination == "control", !is.na(NrFlowers), Variable %in% c("EndSize")) %>% # only control plants, remove if unknown if plant flowered, only one variable
-  group_by(Species, Site, Treatment) %>%
+ProbFlower <- Pollination %>% 
+  # remove Second Flowers
+  filter(!Pollination == "") %>% 
+  filter(!is.na(NrFlowers), 
+         Variable %in% c("EndSize")) %>% # only control plants, remove if unknown if plant flowered, only one variable
+  group_by(Year, Species, Site, Treatment) %>%
   mutate(nPlant = n()) %>% 
-  group_by(Species, Site, Treatment, Flowering) %>%
-  mutate(n = n()) %>%
-  mutate(ProbFl = n / nPlant) %>% 
+  group_by(Year, Species, Site, Treatment, Flowering) %>%
+  mutate(n = n(),
+         ProbFl = n / nPlant) %>% 
   ungroup() %>% 
   # centre P and T-level
-  mutate(DestPLevel.cen = scale(DestPLevel, scale = FALSE)) %>% 
-  mutate(DestTLevel.cen = scale(DestTLevel, scale = FALSE)) %>% 
-  mutate(Treatment = plyr::mapvalues(Treatment, c("Control", "Warmer", "LaterSM", "WarmLate"), c("Control", "Warmer", "Later SM", "Warm & late SM"))) %>%
-  mutate(Treatment = factor(Treatment, levels = c("Control", "Warmer", "Later SM", "Warm & late SM")))
+  mutate(DestPLevel.cen = scale(DestPLevel, scale = FALSE),
+         DestTLevel.cen = scale(DestTLevel, scale = FALSE),
+         value.cen = scale(value, scale = FALSE)) 
+  #mutate(Treatment = plyr::mapvalues(Treatment, c("Control", "Warmer", "LaterSM", "WarmLate"), c("Control", "Warmer", "Later SM", "Warm & late SM"))) %>%
+  #mutate(Treatment = factor(Treatment, levels = c("Control", "Warmer", "Later SM", "Warm & late SM")))
 
+# Plot everything
 SP <- c(LEO = "L. autumnalis", RAN = "R. acris")
 ProbFlowerPlot <- ProbFlower %>% 
   mutate(Species = factor(Species, levels = c("RAN", "LEO"))) %>% 
-  #mutate(Species = plyr::mapvalues(Species, c("LEO", "RAN"), c("L. autumnails", "R. acris"))) %>% 
-  ggplot(aes(x = value, y = ProbFl, color = Treatment)) +
+  ggplot(aes(x = value, y = ProbFl, color = Treatment, linetype = factor(Year))) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "glm", 
               method.args = list(family = "binomial"), 
@@ -39,32 +39,85 @@ ProbFlowerPlot <- ProbFlower %>%
   labs(x = "Longest leaf in cm", y = "Probability of flowering") +
   facet_grid(Species ~ Site, labeller=labeller(Species = SP)) + 
   theme(strip.text.y = element_text(face = "italic"))
-ggsave(ProbFlowerPlot, filename = "FinalFigures/ProbFlowerPlot.jpg", height = 5, width = 8)
+#ggsave(ProbFlowerPlot, filename = "FinalFigures/ProbFlowerPlot.jpg", height = 5, width = 8)
+
+# Check if there is a difference between pollinated and non-pollinated flowers. There is not!
+summary(glm(ProbFl ~ Pollination + Species + Year, ProbFlower, family = "binomial", weights = nPlant))
+
+ggplot(ProbFlower, aes(x = Pollination, y = ProbFl)) +
+  geom_boxplot() +
+  facet_grid(Year ~ Species)
 
 
-# Only controls
-SP <- c(LEO = "L. autumnalis", RAN = "R. acris")
-dfProb <- ProbFlower %>% 
-  filter(Site == "VES") %>%
-  filter(!is.na(value), !is.na(ProbFl)) %>% 
-  group_by(Species) %>% 
-  do(mylogit = glm(Flowering ~ value * Treatment, data = ., family = "binomial"))
+# Only test plants at Veskre - Ranunculus
+dfProbRan <- ProbFlower %>% 
+  filter(Site == "VES",
+         !is.na(value), 
+         Species == "RAN") %>%  
+  group_by(Species, Year) %>% 
+  do(mylogit = glmer(Flowering ~ value * Treatment + (1|NewBlock), data = ., family = "binomial"))
 
-ProbPlot <- augment(dfProb, mylogit) %>% 
-  filter(!(Species == "RAN" & Treatment == "Later SM")) %>% 
+tidy(dfProbRan, mylogit, effect = "fixed") %>% 
+  mutate(estimate = (round(estimate, 2)), std.error = round(std.error, 2), statistic = round(statistic, 2)) %>% 
+  mutate(p.value = round(p.value, 3))
+
+ProbPlotRan <- augment(dfProbRan, mylogit) %>% 
   ggplot(aes(x = value, y = plogis(.fitted), colour = Treatment)) +
-  geom_line() +
-  scale_colour_manual(name = "Treatment", values = c("grey", "red", "blue", "purple")) +
+  geom_line(size = 1) +
+  scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
   labs(x = "Longest leaf in cm", y = "Probability of flowering") +
-  facet_grid( ~ Species, labeller=labeller(Species = SP)) + 
-  theme(strip.text.x = element_text(face = "italic"))
+  facet_grid( ~ Year) 
+  #facet_grid(Year ~ Species, labeller=labeller(Species = SP)) + 
+  #theme(strip.text.x = element_text(face = "italic"))
 ggsave(ProbPlot, filename = "FinalFigures/ProbPlot.jpg", height = 5, width = 8)
 
 
-tidy(dfProb, mylogit) %>% 
+# Only at Veskre - Leontodon 2017
+dfProbLeo <- ProbFlower %>% 
+  filter(Site == "VES",
+         !is.na(value), 
+         Species == "LEO", 
+         Year == 2017) %>%  
+  # fix overdispersion
+  mutate(OD = rnorm(266)) %>% 
+  group_by(Species) %>% 
+  do(mylogit = glmer(Flowering ~ value * Treatment + (1|NewBlock) + (1|OD), data = ., family = "binomial"))
+
+fit <- glmer(Flowering ~ value * Treatment + (1|NewBlock) + (1|OD), data = dfProbLeo, family = "binomial")
+summary(fit2)
+
+ss <- getME(fit,c("theta","fixef"))
+fit2 <- update(fit,start=ss,control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e3)))
+
+tidy(dfProbLeo, mylogit, effect = "fixed") %>% 
   mutate(estimate = (round(estimate, 2)), std.error = round(std.error, 2), statistic = round(statistic, 2)) %>% 
-  mutate(p.value = round(p.value, 3)) %>% 
-  filter(std.error < 100) # remove Later SM for RAN
+  mutate(p.value = round(p.value, 3))
+
+newdata <- ProbFlower %>% 
+  filter(Site == "VES",
+         !is.na(value), 
+         Species == "LEO", 
+         Year == 2017) %>% 
+  distinct(value, Treatment) %>% 
+  mutate(NewBlock = 1,
+         OD = 0,
+         fitted = predict(fit, newdata = newdata, allow.new.levels = TRUE))
+
+#augment(dfProbLeo, mylogit), newdata, allow.new.levels = TRUE) %>% 
+#augment(fit2, effect = "fixed") %>% 
+
+ggplot(newdata, aes(x = value, y = plogis(fitted), colour = Treatment)) +
+  geom_line(size = 1) +
+  scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
+  labs(x = "Longest leaf in cm", y = "Probability of flowering")
+
+
+
+
+
+
+
+
 
 
 
