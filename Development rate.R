8a533eeeee07975a2877cfe5f0a19500e0fe550d
audhalbritter <audhalbritter.com>
2019-12-19 13:41:59 +0100

    fix analysis lme with year as random effect

--- Development rate.R
+++ Development rate.R
@@ -7,8 +7,9 @@ library("tidyverse")
 library("ggforce")
 library("lubridate")
 library("nlme")
+library("lme4")
 
-# Download data from OSF
+# Download data from OSF (does not always work, download by hand will work)
 get_file(node = "nbys2", 
          file = "cleaned_PhenologyPollination_2015_2017.csv", 
          path = "data_cleanded")
@@ -17,9 +18,7 @@ get_file(node = "nbys2",
          file = "DailyAndCumulativeTemp.csv", 
          path = "data_cleanded")
 
-# The palette with grey:
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
+# figure labels
 SP <- c(LEO = "Leontodon autumnalis", RAN = "Ranunculus acris")
 
 dat <- read_csv(file = "data_cleanded/DailyAndCumulativeTemp.csv", col_names = TRUE)
@@ -61,42 +60,23 @@ dat <- dat %>%
          OrigTLevel.cen = scale(OrigTLevel, scale = FALSE),
          DestPLevel.cen = scale(DestPLevel, scale = FALSE),
          DestTLevel.cen = scale(DestTLevel, scale = FALSE))
+  
 
-
-ggplot(dat, aes(x = meanTemp, y = DevelopmentRate)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  facet_grid(~ Species)
-
+### Mixed effects model
 dfPolli <- dat %>% 
   filter(!(Species == "LEO" & Year == 2015)) %>% 
-  group_by(Species, Variable) %>% 
-  do(fit = lm(DevelopmentRate ~ meanTemp, data = .))
-
-tidy(dfPolli, fit, effects = "fixed") %>% 
-  mutate(term = recode(term, "(Intercept)" = "intercept", "meanTemp" = "slope")) %>% 
-  pivot_wider(names_from = term, values_from = c(estimate, std.error, statistic, p.value)) %>% 
-  mutate(thermalThreshold = -estimate_intercept / estimate_slope,
-         heatAccLevel = 1 / estimate_slope) %>% 
-  ggplot(aes(x = Variable, y = thermalThreshold)) +
-  geom_point() +
-  facet_grid(~ Species)
+  group_by(Species, Treatment, Origin, Site, Variable) %>% 
+  #do(fit = lm(DevelopmentRate ~ meanTemp, data = .))
+  do(fit = lme(DevelopmentRate ~ meanTemp, random = ~ 1 | Year, data = .))
 
 
-dfPolli <- dat %>% 
-  filter(!(Species == "LEO" & Year == 2015)) %>% 
-  group_by(Year, Species, Treatment, Origin, Site, Variable) %>% 
-  do(fit = lm(DevelopmentRate ~ meanTemp, data = .))
-  #do(fit = lme(DevelopmentRate ~ meanTemp, random = ~ 1 | NewBlock, data = .))
-
-res <- tidy(dfPolli, fit, effects = "fixed") 
-
-output <- res %>% 
+# Results
+output <- tidy(dfPolli, fit, effects = "fixed") %>% 
   mutate(term = recode(term, "(Intercept)" = "intercept", "meanTemp" = "slope")) %>% 
   pivot_wider(names_from = term, values_from = c(estimate, std.error, statistic, p.value)) %>%
   mutate(thermalThreshold = -estimate_intercept / estimate_slope,
          heatAccLevel = 1 / estimate_slope) %>% 
-  left_join(Snowmelt, by = c("Site", "Year")) %>% 
+  #left_join(Snowmelt, by = c("Site", "Year")) %>% 
   ungroup() %>% 
   mutate(Treatment = plyr::mapvalues(Treatment, c("Control", "Warmer", "LaterSM", "WarmLate"), c("Control", "Warmer", "Later SM", "Warm & late SM"))) %>% 
   mutate(Treatment = factor(Treatment, levels = c("Control", "Warmer", "Later SM", "Warm & late SM"))) %>% 
@@ -106,21 +86,24 @@ output <- res %>%
                              Origin == "SKJ" ~ "Alpine - late",
                              Origin == "VES" ~ "Subalpine - late"))
 
-ggplot(output, aes(x = Variable, y = thermalThreshold, colour = Treatment, shape = Habitat)) +
+## Plot thermal threshold
+ggplot(output, aes(x = Habitat, y = thermalThreshold, colour = Treatment, shape = Habitat)) +
   geom_jitter(size = 3, alpha = 0.8, width = 0.2) +
   scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
   scale_shape_manual(name = "Origin", values = c(17, 18, 15, 16)) +
-  labs(x = "Snowmelt date", y = "Thermal threshold") +
-  facet_grid(Year ~ Species, labeller=labeller(Species = SP))
+  labs(x = "", y = "Thermal threshold") +
+  facet_grid(Variable ~ Species, labeller=labeller(Species = SP), scales = "free_y")
   
 
+## Plot heat use efficiency
 ggplot(output, aes(x = Variable, y = estimate_slope, colour = Treatment, shape = Habitat)) +
   geom_jitter(size = 3, alpha = 0.7) +
   scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
   scale_shape_manual(name = "Origin", values = c(17, 18, 15, 16)) +
-  labs(x = "Snowmelt date", y = "Heat use effeciency") +
+  labs(x = "", y = "Heat use effeciency") +
   facet_grid(Year ~ Species)
 
+## Plot development rates
 dat %>% 
   filter(!(Species == "LEO" & Year == 2015)) %>% 
   ungroup() %>% 
@@ -128,7 +111,7 @@ dat %>%
                              Origin == "RAM" ~ "Subalpine - early",
                              Origin == "SKJ" ~ "Alpine - late",
                              Origin == "VES" ~ "Subalpine - late")) %>% 
-  ggplot(aes(x = meanTemp, y = DevelopmentRate, colour = Treatment, linetype = as.factor(Year), shape = Habitat)) +
+  ggplot(aes(x = meanTemp, y = DevelopmentRate, colour = Treatment, shape = Habitat)) +
   geom_point(aes()) +
   geom_smooth(method = "lm") +
   scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
@@ -136,54 +119,10 @@ dat %>%
   facet_grid(Variable ~ Species)
 
 
-dat %>% 
-  filter(!(Species == "LEO" & Year == 2015), Variable == "Flower") %>% 
-  ungroup() %>% 
-  mutate(Habitat = case_when(Origin == "GUD" ~ "Alpine - early",
-                             Origin == "RAM" ~ "Subalpine - early",
-                             Origin == "SKJ" ~ "Alpine - late",
-                             Origin == "VES" ~ "Subalpine - late")) %>% 
-  ggplot(aes(x = Habitat, y = DevelopmentRate, fill = Treatment)) +
-  geom_sina() +
-  scale_fill_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
-  labs(x = "", y = "Development rate") +
-  facet_grid(Year ~ Species) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
 
-dat %>% 
-  filter(Species == "RAN" & Variable == "Flower") %>% 
-  ungroup() %>% 
-  mutate(Habitat = case_when(Origin == "GUD" ~ "Alpine - early",
-                             Origin == "RAM" ~ "Subalpine - early",
-                             Origin == "SKJ" ~ "Alpine - late",
-                             Origin == "VES" ~ "Subalpine - late")) %>% 
-  ggplot(aes(x = meanTemp, y = DevelopmentRate, colour = Treatment, shape = Habitat)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
-  scale_shape_manual(name = "Origin", values = c(17, 18, 15, 16)) +
-  facet_grid(Year ~ Treatment)
-
-
-dat %>% 
-  filter(Species == "LEO" & Variable == "Flower", Year == 2017) %>% 
-  ungroup() %>% 
-  mutate(Habitat = case_when(Origin == "GUD" ~ "Alpine - early",
-                             Origin == "RAM" ~ "Subalpine - early",
-                             Origin == "SKJ" ~ "Alpine - late",
-                             Origin == "VES" ~ "Subalpine - late")) %>% 
-  ggplot(aes(x = meanTemp, y = DevelopmentRate, colour = Treatment, shape = Habitat)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  scale_colour_manual(name = "Treatment", values = c("#999999", "#E69F00", "#56B4E9", "#D55E00")) +
-  scale_shape_manual(name = "Origin", values = c(17, 18, 15, 16)) +
-  facet_grid( ~ Treatment)
-
-
-
-
+### Other analysis, comparing development rate by treatment and origin
 
 # WARMER
 dfPolli <- dat %>% 
